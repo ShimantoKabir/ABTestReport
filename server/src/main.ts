87c7b346c208a1a33ed9fe8de6272b69c856126c
Shahariar Kabir <maask5534@gmail.com>
2022-10-12 19:14:55 +0600

    give permission to github page

--- server/src/main.ts
+++ server/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'https://shimantokabir.github.io',
     credentials:true
   })
   // @ts-ignore
