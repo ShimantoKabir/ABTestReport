9b6570f46c62d0069a139e180aa13811ba6b0af7
Shahariar Kabir <maask5534@gmail.com>
2022-10-24 20:02:42 +0600

    added

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from "@nestjs/common";
 import * as cookieParser from 'cookie-parser';
 
 const port = 3001;
-
+// added
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
