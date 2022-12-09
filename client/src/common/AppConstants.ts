a1110767dd95d983a88920a65157ea8a331dcfc4
Shahariar Kabir <maask5534@gmail.com>
2022-12-09 21:24:01 +0600

    new build 2

--- client/src/common/AppConstants.ts
+++ client/src/common/AppConstants.ts
@@ -1,23 +1,7 @@
 export default class AppConstants {
 	// public static baseUrl = "http://localhost:3001/";
 	public static baseUrl = "https://ab-test-report-server-production.up.railway.app/";
-	public static getAxiosHeader = () => {
-		return {
-			'Content-Type': 'application/json',
-			'Authorization': GetJwtToken()
-		}
-	};
 	public static loggedInCookieName = "isLoggedIn";
 	public static authTokenCookieName = "authToken";
 	public static refreshTokenCookieName = "refreshToken";
-}
-
-function GetJwtToken(): string {
-	const cookieObj: { isLoggedIn: boolean, jwt: string } =
-		document.cookie.split('; ').reduce((prev: any, current: string) => {
-			const [name, ...value] = current.split('=');
-			prev[name] = value.join('=');
-			return prev;
-		}, {});
-	return cookieObj.jwt;
 }
